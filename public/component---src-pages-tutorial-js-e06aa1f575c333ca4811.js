e388145ae730efb8feab619a23047dda3d4860fd
hamdannurachid <hamdan@sawala.tech>
2020-12-02 21:09:13 +0700

    del

--- public/component---src-pages-tutorial-js-e06aa1f575c333ca4811.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{rUea:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),u=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"Tutorial"}),l.a.createElement("h1",null,"TUTORIAL"))}}}]);
-//# sourceMappingURL=component---src-pages-tutorial-js-e06aa1f575c333ca4811.js.map
--- public/component---src-pages-tutorial-js-e6878ddf4e30671f0aa6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{rUea:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),u=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"Tutorial"}),l.a.createElement("h1",null,"TUTORIAL"))}}}]);
-//# sourceMappingURL=component---src-pages-tutorial-js-e6878ddf4e30671f0aa6.js.map
